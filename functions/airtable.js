57f6435f3085d7548eb5c0b22a36e7624fb92898
David Botros <a01052944@unet.univie.ac.at>
2021-08-12 05:23:02 +0200

    + desc

--- functions/airtable.js
+++ functions/airtable.js
@@ -8,8 +8,8 @@ exports.handler = async (event, context) => {
         const { records } = await airtable.list()
         const products = records.map((product_details) => {
             const { id } = product_details
-            const { name, price } = product_details.fields
-            return { id, name, price }
+            const { name, price,desc } = product_details.fields
+            return { id, name, desc, price }
         })
         return {
             statusCode: 200,
