7703ebd9fcaa28807bad37c58e50bfe38a5cb544
David Botros <a01052944@unet.univie.ac.at>
2021-08-12 05:07:58 +0200

    first commit

---
+++ assets/products.js
@@ -0,0 +1,49 @@
+const items = [
+ {
+    id: 'new',
+    name: 'new',
+    image: {
+      url:
+        'https://dl.airtable.com/.attachments/3245c726ee77d73702ba8c3310639727/f000842b/product-5.jpg',
+    },
+    price: 1.00,
+  },
+  {
+    id: 'new',
+    name: 'new',
+    image: {
+      url:
+        'https://dl.airtable.com/.attachments/3245c726ee77d73702ba8c3310639727/f000842b/product-5.jpg',
+    },
+    price: 1.00,
+    },
+  {
+    id: 'new',
+    name: 'new',
+    image: {
+      url:
+        'https://dl.airtable.com/.attachments/3245c726ee77d73702ba8c3310639727/f000842b/product-5.jpg',
+    },
+    price: 1.00,
+    },
+  {
+    id: 'new',
+    name: 'new',
+    image: {
+      url:
+        'https://dl.airtable.com/.attachments/3245c726ee77d73702ba8c3310639727/f000842b/product-5.jpg',
+    },
+    price: 1.00,
+  },
+  {
+    id: 'new',
+    name: 'new',
+    image: {
+      url:
+        'https://dl.airtable.com/.attachments/3245c726ee77d73702ba8c3310639727/f000842b/product-5.jpg',
+    },
+    price: 1.00,
+  },
+]
+
+module.exports = items
---
+++ functions/airtable.js
@@ -0,0 +1,24 @@
+require('dotenv').config()
+const Airtable = require('airtable-node')
+
+const airtable = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY}).base('appBD8FmwsEWL1jIA').table('test')
+
+exports.handler = async (event, context) => {
+    try {
+        const { records } = await airtable.list()
+        const products = records.map((product_details) => {
+            const { id } = product_details
+            const { name, price } = product_details.fields
+            return { id, name, price }
+        })
+        return {
+            statusCode: 200,
+            body: JSON.stringify(products)
+        }
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body: 'Server Error'
+        }
+    }
+}
